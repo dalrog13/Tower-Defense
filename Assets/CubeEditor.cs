a5c408c7a23cc016410c123e7782438a629c2e85
Dallas <57974653+dalrog13@users.noreply.github.com>
2021-07-27 17:58:39 -0700

    Creating Enemy movement cubes

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
     // Start is called before the first frame update
+    TextMesh textMesh;
     void Start()
     {
         
@@ -17,9 +19,17 @@ public class EditorSnap : MonoBehaviour
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, -5f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+
+        
+        
     }
 }
